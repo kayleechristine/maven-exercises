a5fd8e943081015b46445fa40af90c9c74e35281
Kaylee Morris <kaylee.christine.morris@gmail.com>
2023-01-12 15:05:35 -0600

    finished the dependencies exercise.

---
+++ src/main/java/blah.java
@@ -0,0 +1,12 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class blah {
+
+    public static void main(String[] args) {
+
+        String bleh = "This lesson has no instructions on actually porting Maven in.  ¯\\_(ツ)_/¯";
+        System.out.println(StringUtils.swapCase(bleh));
+
+    }
+
+}
